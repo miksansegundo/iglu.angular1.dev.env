55e5f127c4815b448487a5a22a0ca6e3e83d53b7
miKsansegundo <miguelsansegundo@gmail.com>
2016-11-04 00:10:41 +0700

    Clean deploy

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = (env) => {
     module: {
       loaders: [
         {
-          test: /\.html$/,
+          test: /\.(html|html\.ejs$)$/,
           loader: 'html!html-minify'
         },
         {
