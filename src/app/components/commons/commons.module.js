71710bc739bd42999ceaf0ba605847a72974c444
miKsansegundo <miguelsansegundo@gmail.com>
2016-11-10 04:51:09 +0700

    Test. JSON Server.

--- src/app/components/commons/commons.module.js
+++
@@ -1,9 +0,0 @@
-
-import root from './root/root.component'
-import header from './header/header.component'
-
-export default window.angular
-  .module('commons', [
-    root.name,
-    header.name
-  ])
