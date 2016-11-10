10988505523b17be8c54fdc1c7429a16afd27a32
miKsansegundo <miguelsansegundo@gmail.com>
2016-11-10 15:21:38 +0700

    Json request from db.js

--- test/spec.bundle.js
+++
@@ -1,9 +0,0 @@
-
-import angular from 'angular'
-import mocks from 'angular-mocks'
-import uiRouter from 'angular-ui-router'
-import uiBootstrap from 'angular-ui-bootstrap'
-
-let context = require.context('../src/app', true, /\.spec\.js/)
-
-context.keys().forEach(context)
